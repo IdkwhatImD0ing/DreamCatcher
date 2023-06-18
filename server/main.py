db51d72b6f78369c9b115367d327890d402b9a0c
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2023-06-18 13:24:25 -0700

    Add wav

--- server/main.py
+++ server/main.py
@@ -13,6 +13,58 @@ import ray
 import openai
 from pydantic import BaseModel
 import dotenv
+import ray
+
+runtime_env = {
+    "pip": [
+        "aiohttp==3.8.4",
+        "aiosignal==1.3.1",
+        "anyio==3.7.0",
+        "async-timeout==4.0.2",
+        "asyncio==3.4.3",
+        "attrs==23.1.0",
+        "certifi==2023.5.7",
+        "charset-normalizer==3.1.0",
+        "click==8.1.3",
+        "colorama==0.4.6",
+        "distlib==0.3.6",
+        "exceptiongroup==1.1.1",
+        "fastapi==0.97.0",
+        "filelock==3.12.2",
+        "frozenlist==1.3.3",
+        "grpcio==1.49.1",
+        "h11==0.14.0",
+        "httptools==0.5.0",
+        "hume==0.3.3",
+        "idna==3.4",
+        "jsonschema==4.17.3",
+        "msgpack==1.0.5",
+        "multidict==6.0.4",
+        "numpy==1.25.0",
+        "openai==0.27.8",
+        "packaging==23.1",
+        "platformdirs==3.6.0",
+        "protobuf==4.23.3",
+        "pydantic==1.10.9",
+        "pyrsistent==0.19.3",
+        "python-dotenv==1.0.0",
+        "PyYAML==6.0",
+        "ray==2.5.0",
+        "requests==2.31.0",
+        "sniffio==1.3.0",
+        "starlette==0.27.0",
+        "tqdm==4.65.0",
+        "typing_extensions==4.6.3",
+        "urllib3==2.0.3",
+        "uvicorn==0.22.0",
+        "virtualenv==20.23.1",
+        "watchfiles==0.19.0",
+        "websockets==11.0.3",
+        "whispercpp==0.0.17",
+        "yarl==1.9.2",
+    ]
+}
+# ray.init(runtime_env=runtime_env)
 
 app = FastAPI()
 
@@ -21,6 +73,10 @@ dotenv.load_dotenv()
 
 class Dream(BaseModel):
     content: str
+
+
+class Audio(BaseModel):
+    data: str
 
 
 openai.api_key = environ.get("OPENAI_API_KEY")
@@ -53,20 +109,27 @@ def dream_analysis(dream: Dream):
 
 
 @app.post("/convert_audio")
-def convert_audio(base64_data: str):
+def convert_audio(audio: Audio):
     # Decode base64 data
-    audio_data = base64.b64decode(base64_data)
+    audio_data = base64.b64decode(audio.data)
 
     # Create an in-memory file-like object
     audio_file = io.BytesIO(audio_data)
 
     # Load the audio file and convert to a NumPy array
     sample_rate, audio_array = wavfile.read(audio_file)
+
+    # Save audio array to a WAV file
+    wavfile.write("output.wav", sample_rate, audio_array)
+
     audio_array = audio_array.tolist()
 
-    transcriber = Transcriber.remote("tiny.en")
-    text = transcriber.transcribe.remote(audio_array)  # needs 16k 16bit mono wav
-    output = ray.get(text)
+    # transcriber = Transcriber.remote("tiny.en")
+    transcriber = Transcriber("tiny.en")
+    # text = transcriber.transcribe.remote(audio_array)  # needs 16k 16bit mono wav
+    text = transcriber.transcribe(audio_array)  # needs 16k 16bit mono wav
+    # output = ray.get(text)
+    output = text
     print(output)
 
     return {"transcript": output}
