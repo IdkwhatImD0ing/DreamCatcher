56d5620f7e8db6850c090e5f36bd779a68fe9cd0
Restyled.io <commits@restyled.io>
2023-06-18 20:58:25 +0000

    Restyled by prettier

--- client/app/dashboard/page.jsx
+++ client/app/dashboard/page.jsx
@@ -6,8 +6,8 @@ import CheckLoggedIn from "@/components/CheckLoggedIn";
 const Dashboard = () => {
   <CheckLoggedIn />;
   return (
-    <div className="flex md:flex-row flex-col min-h-[calc(100vh-88px)] md:h-[calc(100vh-88px)]">
-      <div className="flex flex-col justify-center w-full h-full font-semibold md:px-[2rem] px-[1rem] md:py-0 py-[8rem]">
+    <div className="flex min-h-[calc(100vh-88px)] flex-col md:h-[calc(100vh-88px)] md:flex-row">
+      <div className="flex h-full w-full flex-col justify-center px-[1rem] py-[8rem] font-semibold md:px-[2rem] md:py-0">
         <div className="text-[48px]">
           Welcome <span className="text-purple">User</span>
         </div>
@@ -17,14 +17,14 @@ const Dashboard = () => {
           interpretations of your subconscious experiences.
         </div>
         <Link href="/dashboard/record">
-          <button className="font-bold bg-black hover:bg-purple transition-all w-[190px] h-[56px] rounded-[8px] mt-[2rem] text-[#FFFFFF]">
+          <button className="mt-[2rem] h-[56px] w-[190px] rounded-[8px] bg-black font-bold text-[#FFFFFF] transition-all hover:bg-purple">
             Record New Dream
           </button>
         </Link>
       </div>
-      <div className="flex flex-col justify-center w-full h-full font-semibold md:px-[2rem] px-[1rem] md:py-[2rem] py-[8rem]">
+      <div className="flex h-full w-full flex-col justify-center px-[1rem] py-[8rem] font-semibold md:px-[2rem] md:py-[2rem]">
         <div className="text-[24px]">Your Recent Dreams</div>
-        <div className="flex flex-wrap md:gap-[4rem] mx-auto w-full max-w-[960px] overflow-auto gap-[0.5rem] mt-[1rem]">
+        <div className="mx-auto mt-[1rem] flex w-full max-w-[960px] flex-wrap gap-[0.5rem] overflow-auto md:gap-[4rem]">
           <DreamCard />
           <DreamCard />
           <DreamCard />
