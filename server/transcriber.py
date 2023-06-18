ea1f16040923f14d756cae33653ad44bd1028aa4
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2023-06-18 08:30:47 -0700

    Add transcriber

---
+++ server/transcriber.py
@@ -0,0 +1,17 @@
+from whispercpp import Whisper
+
+
+class Transcriber:
+    def __init__(self, model_name):
+        self.whisper = Whisper.from_pretrained(model_name)
+
+    def transcribe_from_file(self, audio_file):
+        text = self.whisper.transcribe_from_file(audio_file)
+        return text
+
+
+if __name__ == "__main__":
+    transcriber = Transcriber("tiny.en")
+    wav_file = "/Users/flerovious/Documents/whisper.cpp/samples/jfk.wav"
+    text = transcriber.transcribe_from_file(wav_file)  # needs 16k 16bit mono wav
+    print(text)
